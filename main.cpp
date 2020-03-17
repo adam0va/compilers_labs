d153f7dd589cb408c578333e73b57177de47cd7f
Irina <irinaadamova555@gmail.com>
2020-03-17 13:31:16 +0300

    make dfa from nfa

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <set>
 #include <queue>
 #include <map>
+#include <utility>
 
 // STEP 1: insert concatenation operator to regular expression
 void addConcatSymbol(std::string &s) {
@@ -151,7 +152,6 @@ public:
 
 	void addState(state *newState, bool isStart, bool isEnd) {
 		states.insert(std::pair<int, state*>(newState->state, newState));
-		//states.push_back(newState);
 		if (isStart) start = newState;
 		if (isEnd) end = newState;
 	}
@@ -278,9 +278,76 @@ NFA postfixToNFA(std::string &postfix) {
 }
 
 // STEP 4: building DFA from NFA
+struct DFAState
+{
+	int state;
+	std::set<int> fromNFAstates;
+	std::map<char, int> transitions;
+	bool isFinal;
+};
+
+DFAState *createDFAState(int state_, std::set<int> fromStates, bool isFinal_) {
+	DFAState *newState = new DFAState;
+	newState->state = state_;
+	newState->fromNFAstates = fromStates;
+	newState->isFinal = isFinal_;
+	return newState;
+}
+
+void printDFAState(DFAState *state) {
+	std::cout << "state " << state->state << " is final? " << (state->isFinal == true) << std::endl;
+	std::cout << "from NFA states: ";
+	for (std::set<int>::iterator itr = state->fromNFAstates.begin(); itr != state->fromNFAstates.end(); ++itr) {
+		std::cout << (*itr) << " ";
+	}
+	std::cout << "\ntrasitions: ";
+	if (state->transitions.empty()) {
+		std::cout << "\n";
+		return;
+	}
+	for (std::map<char, int>::iterator itr = state->transitions.begin(); itr != state->transitions.end(); ++itr) {
+		std::cout << "to: " << (*itr).second << " symbol: " << (*itr).first << '\n';
+	}
+	std::cout << "\n";
+}
+
+class DFA
+{
+public:
+	std::map<int, DFAState*> states;
+	DFAState *start, *end;
+	DFA() {
+		start = NULL;
+		end = NULL;
+	}
+	~DFA() {}
+	void addDFAState(DFAState *newState, bool isStart, bool isFinal) {
+		states.insert(std::pair<int, DFAState*>(newState->state, newState));
+		if (isStart) start = newState;
+		if (isFinal) end = newState;
+	}
+	void printDFA() {
+		std::cout << "DFA: ";
+		for (std::map<int, DFAState*>::iterator itr = states.begin(); itr != states.end(); ++itr) {
+			printDFAState((*itr).second);
+		}
+	}
+
+};
+
+std::set<int> findTransitionsByLetter(std::vector<trasition*> trasitions, char letter) {
+	std::set<int> trasitionsTo;
+
+	for (std::vector<trasition*>::iterator itr = trasitions.begin(); itr < trasitions.end(); ++itr) {
+		if ((*itr)->symbol == letter) {
+			trasitionsTo.insert((*itr)->toState);
+		}
+	}
+	return trasitionsTo;
+}
+
 std::set<int> epsilonClosure(NFA nfa, state *curState) {
 	std::set<int> curEpsilonClosure;
-
 	curEpsilonClosure.insert(curState->state);
 	int epsilonTrasitionsNumber = curState->epsilonTrasitions.size();
 	for (int i = 0; i < epsilonTrasitionsNumber; i++) {
@@ -291,10 +358,94 @@ std::set<int> epsilonClosure(NFA nfa, state *curState) {
 	}
 	std::set<int>::iterator itr;
 	std::cout << "epsilonClosure " << curState->state << ": "; 
-	for (itr = curEpsilonClosure.begin(); itr != curEpsilonClosure.end(); itr++) 
+	for (itr = curEpsilonClosure.begin(); itr != curEpsilonClosure.end(); ++itr) 
         std::cout << (*itr) << ' ';
     std::cout << std::endl;
 	return curEpsilonClosure;
+}
+
+bool isFinal(NFA nfa, std::set<int> states) {
+	for (std::set<int>::iterator itr = states.begin(); itr != states.end(); ++itr) 
+		if (nfa.end->state == (*itr)) 
+			return true;
+	return false;
+}
+
+int stateExcists(DFA &dfa, std::vector<std::pair<int, std::set<int> > > queue, std::set<int> set) {
+	// std::cout << "\ncheck the set: ";
+	// for (std::set<int>::iterator itr = set.begin(); itr != set.end(); ++itr) {
+	// 	std::cout << (*itr) <<  ' ';
+	// }
+	std::cout << '\n';
+	for (std::vector<std::pair<int, std::set<int> > >::iterator queueItr = queue.begin(); 
+			queueItr != queue.end(); ++queueItr) {
+		if (set == (*queueItr).second) {
+			std::cout << "state excists" << std::endl;
+			return (*queueItr).first;
+		}
+	}
+	std::map<int, DFAState*> &states = dfa.states;
+	for (std::map<int, DFAState*>::iterator itr = states.begin(); itr != states.end(); ++itr) {
+		if ((*itr).second->fromNFAstates == set) {
+			std::cout << "state excists" << std::endl;
+			return (*itr).first;
+		}
+	}
+	std::cout << "state does not excist" << std::endl;
+	return -1;
+}
+
+DFA NFAtoDFA(NFA nfa, std::set<char> alfabet) {
+	std::set<int> startState;
+	std::vector<state> DFAStates;
+	std::vector<std::pair<int, std::set<int> > > stateQueue; // поменять очередь на вектор для норм проверки
+	int stateCounter = 0;
+	DFA dfa;
+
+	startState = epsilonClosure(nfa, nfa.start);
+	stateQueue.push_back(std::make_pair(stateCounter, startState));
+	stateCounter++;
+
+	while (not(stateQueue.empty())) { // в каждом состоянии из множества найти все переходы по каждой букве из алфавита
+		std::set<int> currentState = stateQueue.front().second;
+		int nState = stateQueue.front().first;
+		bool isFinal_ = isFinal(nfa, stateQueue.front().second);
+		dfa.addDFAState(createDFAState(stateQueue.front().first, stateQueue.front().second, isFinal_), nState == 0, isFinal_);
+
+		// for each letter in the alfabet
+		for (std::set<char>::iterator alfabetItr = alfabet.begin(); alfabetItr != alfabet.end(); ++alfabetItr) {
+			std::set<int> newState;
+			// for each state of nfa from the set
+			for (std::set<int>::iterator stateItr = currentState.begin(); stateItr != currentState.end(); ++stateItr) {
+				int nfaState = (*stateItr);
+				// get transitions by the letter
+				std::set<int> state_ = findTransitionsByLetter(nfa.states[nfaState]->trasitions, (*alfabetItr));
+				// create new state where all the transitions go
+				newState.insert(state_.begin(), state_.end());
+			}
+
+			if (not(newState.empty())) {
+				// find epsilon closure of these transitions
+				std::set<int> finalSetOfStates;
+				for (std::set<int>::iterator itr = newState.begin(); itr != newState.end(); ++itr) {
+					std::set<int> fromEpsClosure = epsilonClosure(nfa, nfa.states[(*itr)]);
+					finalSetOfStates.insert(fromEpsClosure.begin(), fromEpsClosure.end());
+				}
+				// add new set of NFA states to queue if they haven't been there
+				int n = stateExcists(dfa, stateQueue, finalSetOfStates);
+				if (n >= 0) {
+					dfa.states[nState]->transitions.insert(std::make_pair((*alfabetItr), n));
+				} else {
+					stateQueue.push_back(std::make_pair(stateCounter, finalSetOfStates));
+					dfa.states[nState]->transitions.insert(std::make_pair((*alfabetItr), stateCounter++));
+				}
+			}
+			
+		}
+		stateQueue.erase(stateQueue.begin());
+	}
+	dfa.printDFA();
+	return dfa;
 }
 
 
@@ -303,6 +454,7 @@ int main() {
 	std::string postfix;
 	std::set<char> alfabet;
 	NFA nfa;
+	DFA dfa;
 
 	std::cout << "Enter your regex in infix form: ";
 	std::cin >> regex;
@@ -316,7 +468,8 @@ int main() {
         std::cout << (*itr) << ' ';
     std::cout << std::endl; 
 	nfa = postfixToNFA(postfix);
-	epsilonClosure(nfa, nfa.start);
+	//epsilonClosure(nfa, nfa.start);
+	NFAtoDFA(nfa, alfabet);
 }
 
 
