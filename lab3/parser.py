f218dd3d9eab3e1abccba4e9467bc5eb75eee49a
Irina <irinaadamova555@gmail.com>
2020-05-11 22:58:57 +0300

    add variables if printing needed

--- lab3/parser.py
+++ lab3/parser.py
@@ -18,6 +18,8 @@ class Parser:
 		self.number_of_current_lexem = 0
 		self.length_of_list = len(self.lexems)
 		self.current_lexem = self.lexems[self.number_of_current_lexem]
+		self.print_name_of_functions = False
+		self.print_lexem = False
 
 	def read_program_from_file(filename: str):
 		with open(filename) as file:
@@ -28,10 +30,11 @@ class Parser:
 		self.number_of_current_lexem += 1
 
 		self.current_lexem = self.lexems[self.number_of_current_lexem]
-		print(f'current lexem is: {self.current_lexem}')
+		if self.print_lexem:
+			print(f'Currently examining lexem  "{self.current_lexem}"')
 
 	def print_lexems(self):
-		print(self.lexems)
+		print(f'Lexems: {self.lexems}')
 
 	def __is_identificator(self, lexem):
 		return lexem == 'x' or lexem == 'y' or lexem == 'z'
@@ -46,22 +49,23 @@ class Parser:
 		return lexem == '*' or lexem == '/'
 
 	def __is_constant(self, lexem):
-		print(f'in range? {lexem} {lexem in range(10)}')
+		#print(f'in range? {lexem} {lexem in range(10)}')
 		return lexem in ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
 	def analyse(self):
-		print(f'start')
 		self.program()
 		if self.current_lexem != '_':
 			raise ParserSyntaxError('EOF expected')
-		print(f'good')
+		print(f'Your program is syntactically correct')
 
 	def program(self):
-		print(f'program')
+		if self.print_name_of_functions:
+			print(f'In program')
 		self.block()
 
 	def block(self):
-		print(f'block')
+		if self.print_name_of_functions:
+			print(f'In block')
 		if self.lexems[self.number_of_current_lexem] == '{':
 			self.next()
 			self.list_of_operators()
@@ -73,7 +77,8 @@ class Parser:
 			raise ParserSyntaxError('{ expected')
 
 	def list_of_operators(self):
-		print(f'list_of_operators')
+		if self.print_name_of_functions:
+			print(f'In list_of_operators')
 		if self.__is_identificator(self.current_lexem) or self.current_lexem == '{':
 			self.operator()
 			if self.current_lexem == ';':
@@ -82,7 +87,8 @@ class Parser:
 			raise ParserSyntaxError('Operator expected')
 
 	def operator(self):
-		print(f'operator')
+		if self.print_name_of_functions:
+			print(f'In operator')
 		if self.current_lexem == '{':
 			self.next()
 			self.list_of_operators()
@@ -99,7 +105,8 @@ class Parser:
 			raise ParserSyntaxError('Operator expected')
 
 	def tail(self):
-		print(f'tail')
+		if self.print_name_of_functions:
+			print(f'In tail')
 		if self.current_lexem == ';':
 			self.next()
 			self.operator()
@@ -109,40 +116,46 @@ class Parser:
 			raise ParserSyntaxError('Tail expected')
 
 	def expression(self):
-		print(f'expression')
+		if self.print_name_of_functions:
+			print(f'In expression')
 		self.arithmetic_expression()
 		if self.__is_operation_of_relation(self.current_lexem):
 			self.operation_of_relation()
 			self.arithmetic_expression()
 
 	def arithmetic_expression(self):
-		print(f'arithmetic_expression')
+		if self.print_name_of_functions:
+			print(f'In arithmetic_expression')
 		self.term()
 		if self.__is_operation_of_addition(self.current_lexem):
 			self.arithmetic_expression_()
 
 	def arithmetic_expression_(self):
-		print(f'arithmetic_expression_')
+		if self.print_name_of_functions:
+			print(f'In arithmetic_expression_')
 		self.operation_of_addition()
 		self.term()
 		if self.__is_operation_of_addition(self.current_lexem):
 			self.arithmetic_expression_()
 
 	def term(self):
-		print(f'term')
+		if self.print_name_of_functions:
+			print(f'In term')
 		self.factor()
 		if self.__is_operation_of_multiplication(self.current_lexem):
 			self.term_()
 
 	def term_(self):
-		print(f'term_')
+		if self.print_name_of_functions:
+			print(f'In term_')
 		self.operation_of_multiplication()
 		self.factor()
 		if self.__is_operation_of_multiplication(self.current_lexem):
 			self.term_()
 
 	def factor(self):
-		print(f'factor')
+		if self.print_name_of_functions:
+			print(f'In factor')
 		if self.__is_identificator(self.current_lexem):
 			self.identificator()
 		elif self.__is_constant(self.current_lexem):
@@ -159,35 +172,40 @@ class Parser:
 
 
 	def operation_of_relation(self):
-		print(f'operation_of_relation')
-		if self.__is_operation_of_realtion(self.current_lexem):
+		if self.print_name_of_functions:
+			print(f'In operation_of_relation')
+		if self.__is_operation_of_relation(self.current_lexem):
 			self.next()
 		else:
 			raise ParserSyntaxError('Operation of relation expected')
 
 	def operation_of_addition(self):
-		print(f'operation_of_addition')
+		if self.print_name_of_functions:
+			print(f'In operation_of_addition')
 		if self.__is_operation_of_addition(self.current_lexem):
 			self.next()
 		else:
 			raise ParserSyntaxError('Operation of addition expected')
 
 	def operation_of_multiplication(self):
-		print(f'operation_of_multiplication')
+		if self.print_name_of_functions:
+			print(f'In operation_of_multiplication')
 		if self.__is_operation_of_multiplication(self.current_lexem):
 			self.next()
 		else:
 			raise ParserSyntaxError('Operation of multiplication expected')
 
 	def constant(self):
-		print(f'constant')
+		if self.print_name_of_functions:
+			print(f'In constant')
 		if self.__is_constant(self.current_lexem):
 			self.next()
 		else:
 			raise ParserSyntaxError('Constant expected')
 
 	def identificator(self):
-		print(f'identificator')
+		if self.print_name_of_functions:
+			print(f'In identificator')
 		if self.__is_identificator(self.current_lexem):
 			self.next()
 		else:
